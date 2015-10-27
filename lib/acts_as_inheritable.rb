ed749e8a98ef6317c1fa3814345bf06ff08ea05a
Esteban Arango Medina <marranoparael31@gmail.com>
2015-10-26 19:30:42 -0500

    Fix issue where `associations` OR `attributes` are nil

--- lib/acts_as_inheritable.rb
+++ lib/acts_as_inheritable.rb
@@ -20,7 +20,7 @@ module ActsAsInheritable
       # relations defined on `INHERITABLE_ASSOCIATIONS`. For each instance on
       # each relation it re-creates it.
       def inherit_relations(model_parent = send(:parent), current = self)
-        if model_parent && model_parent.class.method_defined?(:inheritable_configuration)
+        if model_parent && model_parent.class.method_defined?(:inheritable_configuration) && model_parent.class::inheritable_configuration[:associations]
           model_parent.class::inheritable_configuration[:associations].each do |relation|
             parent_relation = model_parent.send(relation)
             relation_instances = parent_relation.respond_to?(:each) ? parent_relation : [parent_relation].compact
@@ -35,8 +35,9 @@ module ActsAsInheritable
         new_relation = relation_instance.dup
         belongs_to_associations_names = model_parent.class.reflect_on_all_associations(:belongs_to).collect(&:name)
         saved =
+          # Is a `belongs_to` association
           if belongs_to_associations_names.include?(relation.to_sym)
-            # Is a `belongs_to` association
+            # You can define your own 'dup' method with a `duplicate!` signature
             new_relation = relation_instance.duplicate! if relation_instance.respond_to?(:duplicate!)
             current.send("#{relation}=", new_relation)
             current.save
@@ -70,7 +71,7 @@ module ActsAsInheritable
       end
 
       def inherit_attributes(force = false, not_force_for=[])
-        if has_parent?
+        if has_parent? && self.class.inheritable_configuration[:attributes]
           # Attributes
           self.class.inheritable_configuration[:attributes].each do |attribute|
             current_val = send(attribute)
